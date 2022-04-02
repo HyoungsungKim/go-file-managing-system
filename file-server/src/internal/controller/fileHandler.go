2693237f92f4b7f36c22ec93e876fe5587efe97a
HyoungsungKim <rktkek456@gmail.com>
2022-04-02 21:11:59 +0900

    Add check file exist before create to prevent replace

--- file-server/src/internal/controller/fileHandler.go
+++ file-server/src/internal/controller/fileHandler.go
@@ -1,6 +1,7 @@
 package controller
 
 import (
+	"errors"
 	"log"
 	"net/http"
 	"os"
@@ -14,26 +15,7 @@ type FileHandler struct {
 	*gin.Context
 }
 
-// create function lets client upload a file to `dst`.
-// param `dst` denotes destination directory of created file.
-//
-// example params: dst="storage/users/USER_NAME/"
-func (fileHandler *FileHandler) create(dst string) (httpStatusCode, error) {
-	file, _ := fileHandler.FormFile("file")
-	log.Println(file.Filename)
-
-	if _, err := os.Stat(dst); err != nil {
-		os.Mkdir(dst, os.ModePerm)
-	}
-
-	if err := fileHandler.SaveUploadedFile(file, dst+file.Filename); err != nil {
-		return http.StatusBadRequest, err
-	} else {
-		return http.StatusOK, nil
-	}
-}
-
-// read function lets client check file exists from `src`.
+// checExist function lets client check file exists from `src`.
 // param 'src' denotes source directory of downloading file.
 //
 // example params: fileName="helloFile.txt", src="storage/users/USER_NAME/"
@@ -42,6 +24,29 @@ func (fileHandler *FileHandler) checkExist(fileName string, src string) (httpSta
 	log.Println(filePath)
 
 	if _, err := os.Stat(filePath); err != nil {
+		return http.StatusBadRequest, err
+	} else {
+		return http.StatusOK, nil
+	}
+}
+
+// create function lets client upload a file to `dst`.
+// param `dst` denotes destination directory of created file.
+//
+// example params: dst="storage/users/USER_NAME/"
+func (fileHandler *FileHandler) create(dst string) (httpStatusCode, error) {
+	file, _ := fileHandler.FormFile("file")
+	filePath := strings.Join([]string{dst, file.Filename}, "")
+	log.Println(filePath)
+
+	// If dst directory does not exits, generate
+	if _, err := os.Stat(filePath); err == nil {
+		return http.StatusBadRequest, errors.New("THIS FILE NAME ALREADY EXISTS")
+	} else {
+		os.Mkdir(dst, os.ModePerm)
+	}
+
+	if err := fileHandler.SaveUploadedFile(file, filePath); err != nil {
 		return http.StatusBadRequest, err
 	} else {
 		return http.StatusOK, nil
